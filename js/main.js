0596d98f4839a8b5a53e2472c89fa1604a633959
MonaShoar <112824105+MonaShoar@users.noreply.github.com>
2022-12-12 03:52:29 -0500

    2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function myJs1() {
-    alert("YOU BUY STRAWBERRY CAKE FROM SWEET ART");
+    alert("YOUR ORDER HAVE BEEN RECEIVED");
     alert("THANK YOU FOR YOUR PURCHASE!");
 
 }
 function myJs2() {
-    alert("YOU BUY CHEESE CAKE FROM SWEET ART");
+    alert("YOUR ORDER HAVE BEEN RECEIVED");
     alert("THANK YOU FOR YOUR PURCHASE!");
 
 }
 function myJs3() {
-    alert("YOU BUY CHOCOLATE CAKE FROM SWEET ART");
+    alert("YOUR ORDER HAVE BEEN RECEIVED");
     alert("THANK YOU FOR YOUR PURCHASE!");
 
 }
