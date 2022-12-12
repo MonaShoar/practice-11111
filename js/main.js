5e76a5ac0b53d2c84293e360ebabeecdff328d04
MonaShoar <112824105+MonaShoar@users.noreply.github.com>
2022-12-12 03:48:57 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function myJs1() {
+    alert("YOU BUY STRAWBERRY CAKE FROM SWEET ART");
+    alert("THANK YOU FOR YOUR PURCHASE!");
+
+}
+function myJs2() {
+    alert("YOU BUY CHEESE CAKE FROM SWEET ART");
+    alert("THANK YOU FOR YOUR PURCHASE!");
+
+}
+function myJs3() {
+    alert("YOU BUY CHOCOLATE CAKE FROM SWEET ART");
+    alert("THANK YOU FOR YOUR PURCHASE!");
+
+}
+
+
